80f51b95f22258272ea8d2396c8ea37f3cd1de64
Dimitri Georganas <dimitri.georganas@gmail.com>
2012-07-23 20:56:49 +0200

    Initial commit

---
+++ railsapps/bbc/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bbc::Application.config.secret_token = '149904c94499ebc6623f12bfa4d1108fc7188ccc60b19c7d3b0055d812da9f0b6df9217e1162c1f9b8ec70be2ccac36c8ddc378d165d79507a63fad84dccab65'
